95fb7d298d41ff49a2ded32cfc6fa98a93f99847
Noe Huaccharaque <noemk2@gmail.com>
2019-10-03 06:17:31 -0500

    agregando funcinalidad de click

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -7,9 +7,15 @@
 
       <hr class="my-4" />
 
-      <p v-for="(answer, index) in answers" :key="index">
-        {{ answer }}
-      </p>
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+        >
+          {{ answer }}
+        </b-list-group-item>
+      </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button @click="next" variant="success" href="#">Next</b-button>
@@ -23,16 +29,34 @@ export default {
     currentQuestion: Object,
     next: Function,
   },
+  data() {
+    return {
+      selectedIndex: null,
+    };
+  },
   mounted() {
     console.log(this.currentQuestion);
   },
   computed: {
     answers() {
       let answers = [...this.currentQuestion.incorrect_answers];
-      return answers.push(this.currentQuestion.correct_answer);
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    },
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.btn {
+  margin: 0 5px;
+}
+</style>
